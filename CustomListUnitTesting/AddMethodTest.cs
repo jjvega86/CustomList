75f5741579b16047466a41a7c593a729aca6fedc
jjvega86 <jj.vega86@gmail.com>
2020-08-24 15:24:18 -0400

    Finished test methods for AddMethodTest class.

--- CustomListUnitTesting/AddMethodTest.cs
+++ CustomListUnitTesting/AddMethodTest.cs
@@ -7,20 +7,9 @@ namespace CustomListUnitTesting
     [TestClass]
     public class AddMethodTest
     {
-        // test method 1
-        // test method 2
-        // test method 3
-        // test method 4
-        // test method 5
-
-        //Arrange
-
-        //Act
-
-        //Assert
-
+        
         [TestMethod]
-        public void Execute_AddOneItem_IntValue_CountEqualsOne()
+        public void Execute_AddOneItem_CountEqualsOne()
         {
             //add a single item to the list
 
@@ -36,13 +25,13 @@ namespace CustomListUnitTesting
             actual = newIntList.Count;
 
             //Assert
-            //checks to see that the instantiated list is no longer null
+            //checks to see that the Count is 1
             Assert.Equals(expected, actual);
         }
 
         [TestMethod]
 
-        public void Execute_AddTwoItems_IntValue_CountEqualsTwo()
+        public void Execute_AddTwoItems_CountEqualsTwo()
         {
             //Arrange
             // add two items to the list
@@ -53,43 +42,167 @@ namespace CustomListUnitTesting
             CustomList<int> newIntList = new CustomList<int>();
 
             //Act
-            newIntList.Add(itemOne, itemTwo);
+            newIntList.Add(itemOne); 
+            newIntList.Add(itemTwo);
             actual = newIntList.Count;
 
             //Assert
+            //checks that the expected and actual Counts equal 2
             Assert.Equals(expected, actual);
 
         }
 
-        public void TestMethod3()
+        [TestMethod]
+        public void Execute_AddTwoItems_CapacityEquals4()
         {
             //Arrange
+            int itemOne = 3;
+            int itemTwo = 4;
+            int expected = 4;
+            int actual = 0;
+            CustomList<int> newIntList = new CustomList<int>();
 
             //Act
+            newIntList.Add(itemOne);
+            newIntList.Add(itemTwo);
+            actual = newIntList.Capacity;
 
             //Assert
+            //checks that the expected and actual capacity equal 4 when two items are added
+            Assert.Equals(expected, actual);
+
 
         }
 
-        public void TestMethod4()
+        [TestMethod]
+
+        public void Execute_AddSixItems_CapacityEquals8()
         {
             //Arrange
+            int itemOne = 3;
+            int itemTwo = 4;
+            int itemThree = 7;
+            int itemFour = 8;
+            int itemFive = 3;
+            int itemSix = 12;
+            int expected = 8;
+            int actual = 0;
+            CustomList<int> newIntList = new CustomList<int>();
 
             //Act
+            newIntList.Add(itemOne);
+            newIntList.Add(itemTwo);
+            newIntList.Add(itemThree);
+            newIntList.Add(itemFour);
+            newIntList.Add(itemFive);
+            newIntList.Add(itemSix);
+            actual = newIntList.Capacity;
 
             //Assert
+            // checks that the expected and actual capacity is 8, after enough items are added to array to double capacity
+            Assert.Equals(expected, actual);
+            
 
         }
 
-        public void TestMethod5()
+        [TestMethod]
+        public void Execute_AddSixItems_IndexZeroIsThree()
         {
             //Arrange
+            int itemOne = 3;
+            int itemTwo = 4;
+            int itemThree = 7;
+            int itemFour = 8;
+            int itemFive = 3;
+            int itemSix = 12;
+
+            int expected = 3;
+            int actual = 0;
+            CustomList<int> newIntList = new CustomList<int>();
 
             //Act
+            newIntList.Add(itemOne);
+            newIntList.Add(itemTwo);
+            newIntList.Add(itemThree);
+            newIntList.Add(itemFour);
+            newIntList.Add(itemFive);
+            newIntList.Add(itemSix);
+            actual = newIntList[0];
+           
 
             //Assert
+            // checks index 0 for correct expected value of 3
+            Assert.Equals(expected, actual);
+            
+
+
+        }
+
+        [TestMethod]
+        public void Execute_AddSixItems_IndexTwoIsSeven()
+        {
+            //Arrange
+            int itemOne = 3;
+            int itemTwo = 4;
+            int itemThree = 7;
+            int itemFour = 8;
+            int itemFive = 3;
+            int itemSix = 12;
+
+            int expected = 7;
+            int actual = 0;
+            CustomList<int> newIntList = new CustomList<int>();
+
+            //Act
+            newIntList.Add(itemOne);
+            newIntList.Add(itemTwo);
+            newIntList.Add(itemThree);
+            newIntList.Add(itemFour);
+            newIntList.Add(itemFive);
+            newIntList.Add(itemSix);
+            actual = newIntList[2];
+
+
+            //Assert
+            // checks index 2 for correct expected value of 7
+            Assert.Equals(expected, actual);
+
+
+
+        }
+
+        [TestMethod]
+        public void Execute_AddSixItems_IndexFiveIsTwelve()
+        {
+            //Arrange
+            int itemOne = 3;
+            int itemTwo = 4;
+            int itemThree = 7;
+            int itemFour = 8;
+            int itemFive = 3;
+            int itemSix = 12;
+
+            int expected = 3;
+            int actual = 0;
+            CustomList<int> newIntList = new CustomList<int>();
+
+            //Act
+            newIntList.Add(itemOne);
+            newIntList.Add(itemTwo);
+            newIntList.Add(itemThree);
+            newIntList.Add(itemFour);
+            newIntList.Add(itemFive);
+            newIntList.Add(itemSix);
+            actual = newIntList[5];
+
+
+            //Assert
+            // checks index 5 for correct expected value of 12
+            Assert.Equals(expected, actual);
+
+
 
         }
     }
 }
-}
+
--- Custom_ListProject/CustomList.cs
+++ Custom_ListProject/CustomList.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,57 @@ namespace Custom_ListProject
 {
     public class CustomList <T>
     {
+        T[] _items; // underscore because Microsoft naming conventions for private member variables
+
+        int _capacity;
+        int _count;
+
+        public int Count // public read only property for CustomList. 
+        {
+            get
+            {
+                return _count;
+            }
+        }
+
+        public int Capacity // public read and set property for CustomList
+        {
+            get
+            {
+                return _capacity;
+            }
+            set
+            {
+                _capacity = value;
+            }
+        }
+
+        public CustomList()
+        {
+            _count = 0;
+            _capacity = 4;
+
+            _items = new T[_capacity];
+        }
+
+
+
+
+        public void Add(T item)
+        {
+            T[] tempArray = new T[];
+            _items[_count] = item;
+            _count++;
+
+            if (_count == _capacity)
+            {
+                //need to create a new array
+                //three buckets
+                //create a temporary array to transfer values
+                //then a new array with the new capacity    
+            }
+
+        }
 
 
 
