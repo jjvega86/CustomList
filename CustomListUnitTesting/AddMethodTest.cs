68feadbfe61e05400cec2442f1a1282d6c065490
jjvega86 <jj.vega86@gmail.com>
2020-08-24 12:39:18 -0400

    Init and created first classes for CustomList test methods

---
+++ CustomListUnitTesting/AddMethodTest.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Custom_ListProject;
+
+namespace CustomListUnitTesting
+{
+    [TestClass]
+    public class AddMethodTest
+    {
+        // test method 1
+        // test method 2
+        // test method 3
+        // test method 4
+        // test method 5
+
+        //Arrange
+
+        //Act
+
+        //Assert
+
+        [TestMethod]
+        public void Execute_AddOneItem_IntValue_NewListIsNotNull()
+        {
+            //add a single item to the list
+
+            //Arrange
+            int itemOne = 3;
+            CustomList<int> newIntList = new CustomList<int>();
+
+            //Act
+            //calls the Add method from CustomList to add the variable '3' to the list
+            newIntList.Add(itemOne);
+
+            //Assert
+            //checks to see that the instantiated list is no longer null
+            Assert.IsNotNull(newIntList);
+        }
+
+        [TestMethod]
+
+        public void TestMethod2()
+        {
+            //Arrange
+            // add two items to the list
+
+            //Act
+
+            //Assert
+
+        }
+
+        public void TestMethod3()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+
+        }
+
+        public void TestMethod4()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+
+        }
+
+        public void TestMethod5()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+
+        }
+    }
+}
+}
