b1a5ed089ea5219f176f06fa504ba057fa98579a
jjvega86 <jj.vega86@gmail.com>
2020-08-25 14:25:05 -0400

    Finished test methods for ToStringMethodTest.cs

--- CustomListUnitTesting/ToStringMethodTest.cs
+++ CustomListUnitTesting/ToStringMethodTest.cs
@@ -8,7 +8,7 @@ namespace CustomListUnitTesting
     public class ToStringMethodTest
     {
         [TestMethod]
-        public void ExecuteCombineTwoElementsIntoString_ActualEquals34()
+        public void ExecuteCombineTwoIntElementsIntoString_ActualEquals34()
         {
             // checks that two elements are combined into a single string
             // Arrange
@@ -19,7 +19,6 @@ namespace CustomListUnitTesting
             CustomList<int> newIntList = new CustomList<int>();
 
             // Act
-            // calls the Add method from CustomList to add the variable '3' to the list
             newIntList.Add(itemOne);
             newIntList.Add(itemTwo);
             actual = newIntList.ToString();
@@ -28,5 +27,81 @@ namespace CustomListUnitTesting
             // checks to see that expected actual value is "34", the combination of both items in array
             Assert.AreEqual(expected, actual);
         }
+
+        public void ExecuteCombineSixIntElementsIntoString_ActualEquals123456()
+        {
+            // checks that six elements are combined into a single string
+            // Arrange
+            int itemOne = 1;
+            int itemTwo = 2;
+            int itemThree = 3;
+            int itemFour = 4;
+            int itemFive = 5;
+            int itemSix = 6;
+            string expected = "123456";
+            string actual = "";
+            CustomList<int> newIntList = new CustomList<int>();
+
+            // Act
+            newIntList.Add(itemOne);
+            newIntList.Add(itemTwo);
+            newIntList.Add(itemThree);
+            newIntList.Add(itemFour);
+            newIntList.Add(itemFive);
+            newIntList.Add(itemSix);
+            actual = newIntList.ToString();
+
+            // Assert
+            // checks to see that expected actual value is "123456", the combination of all items in array
+            Assert.AreEqual(expected, actual);
+        }
+
+        public void ExecuteCombineSixStringElementsIntoString_ActualEquals123456()
+        {
+            // checks that six elements are combined into a single string
+            // Arrange
+            string itemOne = "1";
+            string itemTwo = "2";
+            string itemThree = "3";
+            string itemFour = "4";
+            string itemFive = "5";
+            string itemSix = "6";
+            string expected = "123456";
+            string actual = "";
+            CustomList<string> newStringList = new CustomList<string>();
+
+            // Act
+            newStringList.Add(itemOne);
+            newStringList.Add(itemTwo);
+            newStringList.Add(itemThree);
+            newStringList.Add(itemFour);
+            newStringList.Add(itemFive);
+            newStringList.Add(itemSix);
+            actual = newStringList.ToString();
+
+            // Assert
+            // checks to see that expected actual value is "123456", the combination of all string items in array
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ExecuteRunToStringOnEmptyList_ActualEquals0000()
+        {
+            // runs ToString on an empty method
+            // Arrange
+            string expected = "0000";
+            string actual = "";
+            CustomList<int> newIntList = new CustomList<int>();
+
+            // Act
+            
+            actual = newIntList.ToString();
+
+            // Assert
+            // checks to see that expected actual value "0000" since a new list has capacity of 4 with default values 0
+            Assert.AreEqual(expected, actual);
+        }
+
+
     }
 }
