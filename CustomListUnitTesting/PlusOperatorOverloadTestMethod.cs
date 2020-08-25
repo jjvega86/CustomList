a0c63fcc7c107b1aadc8d343b3533b79ceeec117
jjvega86 <jj.vega86@gmail.com>
2020-08-25 16:01:05 -0400

    Added first test method to PlusOperatorOverloadTestMethod.cs

--- CustomListUnitTesting/PlusOperatorOverloadTestMethod.cs
+++ CustomListUnitTesting/PlusOperatorOverloadTestMethod.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Custom_ListProject;
 
 namespace CustomListUnitTesting
 {
@@ -7,7 +8,46 @@ namespace CustomListUnitTesting
     public class PlusOperatorOverloadTestMethod
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ExecuteAddTwoListsOneValueEach_ActualEquals12()
+        {
+            // adds two lists together with one element in each list
+            // Arrange
+
+            CustomList<int> expected = new CustomList<int>();
+            expected.Add(1);
+            expected.Add(2);
+            CustomList<int> actual = new CustomList<int>();
+            CustomList<int> list1 = new CustomList<int>();
+            list1.Add(1);
+            CustomList<int> list2 = new CustomList<int>();
+            list2.Add(2);
+
+            // Act
+
+            actual = list1 + list2;
+
+            // Assert
+            // checks to see that expected actual value is 12, a combination of both arrays list1 and list2
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod5()
         {
         }
     }
